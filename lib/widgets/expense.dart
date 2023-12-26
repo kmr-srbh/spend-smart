90e86d050a6727b50e9eaafe9a3b4fdc72f78d4c
Saurabh Kumar <developer.saurabh@outlook.com>
2023-12-26 10:03:42 +0530

    Expense card style changes

--- lib/widgets/expense.dart
+++ lib/widgets/expense.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
+enum Category {
+  food,
+  work,
+  travel,
+}
+
+Map<Category, IconData> categoryIcons = {
+  Category.food: Icons.fastfood_rounded,
+  Category.work: Icons.laptop_rounded,
+  Category.travel: Icons.directions_walk_rounded,
+};
+
 class Expense extends StatelessWidget {
   const Expense(
       {super.key,
       required this.title,
       required this.amount,
       required this.date,
-      required this.time});
+      required this.time,
+      required this.category});
 
   final String title;
   final int amount;
   final DateTime date;
   final DateTime time;
+  final Category category;
 
   String get formattedDate => '${date.day}-${date.month}-${date.year}';
   String get formattedTime => '${time.hour}:${time.minute}';
@@ -23,11 +37,17 @@ class Expense extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(title,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  )),
+              Row(
+                children: [
+                  Icon(categoryIcons[category]),
+                  const SizedBox(width: 8),
+                  Text(title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ],
+              ),
               const SizedBox(height: 8),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -12,10 +12,26 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   final List<Expense> _registeredExpenses = [
     Expense(
-        title: 'Flutter Course',
-        amount: 389,
-        date: DateTime.now(),
-        time: DateTime.now())
+      title: 'Flutter Course',
+      amount: 389,
+      date: DateTime.now(),
+      time: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Samosa',
+      amount: 14,
+      date: DateTime.now(),
+      time: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Metro fare',
+      amount: 15,
+      date: DateTime.now(),
+      time: DateTime.now(),
+      category: Category.travel,
+    )
   ];
 
   @override
