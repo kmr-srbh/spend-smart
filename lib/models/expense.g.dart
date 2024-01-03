032ec86204c40156b84ae66eeeef5bb52db38160
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-03 16:10:44 +0530

    `Hive` data adapters

---
+++ lib/models/expense.g.dart
@@ -0,0 +1,136 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'expense.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ExpenseAdapter extends TypeAdapter<Expense> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Expense read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Expense(
+      name: fields[0] as String,
+      amount: fields[1] as int,
+      category: fields[2] as Category,
+      date: fields[3] as DateTime,
+      time: fields[4] as TimeOfDay,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Expense obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.amount)
+      ..writeByte(2)
+      ..write(obj.category)
+      ..writeByte(3)
+      ..write(obj.date)
+      ..writeByte(4)
+      ..write(obj.time);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ExpenseAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CategoryAdapter extends TypeAdapter<Category> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Category read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return Category.grocery;
+      case 1:
+        return Category.work;
+      case 2:
+        return Category.travel;
+      case 3:
+        return Category.leisure;
+      case 4:
+        return Category.bills;
+      case 5:
+        return Category.medical;
+      default:
+        return Category.grocery;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, Category obj) {
+    switch (obj) {
+      case Category.grocery:
+        writer.writeByte(0);
+        break;
+      case Category.work:
+        writer.writeByte(1);
+        break;
+      case Category.travel:
+        writer.writeByte(2);
+        break;
+      case Category.leisure:
+        writer.writeByte(3);
+        break;
+      case Category.bills:
+        writer.writeByte(4);
+        break;
+      case Category.medical:
+        writer.writeByte(5);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TimeOfDayAdapter extends TypeAdapter<TimeOfDay> {
+  @override
+  final typeId = 101;
+
+  @override
+  void write(BinaryWriter writer, TimeOfDay obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.hour)
+      ..writeByte(1)
+      ..write(obj.minute);
+  }
+
+  @override
+  TimeOfDay read(BinaryReader reader) {
+    var numOfFields = reader.readByte();
+    var fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TimeOfDay(hour: fields[0] as int, minute: fields[1] as int);
+  }
+}
