d1e3c7dd15454391e53e18693023343cfcf9d1b8
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-03 16:09:21 +0530

    Added `Hive` database

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'package:spend_smart/widgets/home.dart';
+import 'package:spend_smart/models/expense.dart';
 
-void main() {
+import 'package:spend_smart/widgets/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final Directory appDocumentsDirectory =
+      await getApplicationDocumentsDirectory();
+  Hive
+    ..init(appDocumentsDirectory.path)
+    ..registerAdapter(ExpenseAdapter())
+    ..registerAdapter(CategoryAdapter())
+    ..registerAdapter(TimeOfDayAdapter());
+
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void dispose() {
+    Hive.box('expenses').compact();
+    Hive.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +49,33 @@ class MainApp extends StatelessWidget {
       builder: (lightDynamic, darkDynamic) => AnnotatedRegion(
         value: SystemUiOverlayStyle(
           systemNavigationBarColor: isDarkMode
-              ? ThemeData.dark().scaffoldBackgroundColor
-              : ThemeData.light().scaffoldBackgroundColor,
+              ? ElevationOverlay.applySurfaceTint(
+                  Theme.of(context).colorScheme.onSurface,
+                  Theme.of(context).primaryColorDark,
+                  3.0)
+              : ElevationOverlay.applySurfaceTint(
+                  Theme.of(context).colorScheme.surface,
+                  Theme.of(context).colorScheme.onSurface,
+                  3.0),
         ),
         child: MaterialApp(
-          home: const Scaffold(
-            body: Home(),
+          home: FutureBuilder(
+            future: Hive.openBox('expenses'),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                } else {
+                  return const HomeScreen();
+                }
+              } else {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  ),
+                );
+              }
+            },
           ),
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
