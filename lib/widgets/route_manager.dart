fb26b3c1e4f88f0805f3c97cdab026c363fa6f0b
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-06 13:37:23 +0530

    Internal data handling changes

--- lib/widgets/route_manager.dart
+++ lib/widgets/route_manager.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:spend_smart/widgets/add_expense_form.dart';
 import 'package:spend_smart/widgets/past_expenses.dart';
 import 'package:spend_smart/widgets/today_expenses.dart';
 
@@ -13,44 +12,21 @@ class RouteManager extends StatefulWidget {
 
 class _RouteManagerState extends State<RouteManager> {
   int routeIndex = 0;
-  final List<Widget> routes = const [TodayExpenses(), PastExpenses()];
-
-  FloatingActionButton? addExpenseFAB;
-
-  @override
-  void initState() {
-    super.initState();
-    addExpenseFAB = FloatingActionButton.extended(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (dialogContext) => const AlertDialog(
-            title: Text('Add expense'),
-            content: SingleChildScrollView(
-              child: AddExpenseForm(),
-            ),
-          ),
-        );
-      },
-      label: const Text('Add expense'),
-      icon: const Icon(Icons.add_rounded),
-    );
-  }
+  final List<Widget> routes = [TodayExpenses(), const PastExpenses()];
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(),
         body: routes[routeIndex],
-        floatingActionButton: routeIndex == 0 ? addExpenseFAB : null,
         bottomNavigationBar: NavigationBar(
           destinations: const [
             NavigationDestination(
-                icon: Icon(Icons.account_balance_wallet_outlined),
-                selectedIcon: Icon(Icons.account_balance_wallet_rounded),
+                icon: Icon(Icons.receipt_outlined),
+                selectedIcon: Icon(Icons.receipt_rounded),
                 label: "Today's Expenses"),
             NavigationDestination(
-                icon: Icon(Icons.work_history_outlined),
-                selectedIcon: Icon(Icons.work_history_rounded),
+                icon: Icon(Icons.receipt_long_outlined),
+                selectedIcon: Icon(Icons.receipt_long),
                 label: "Past Expenses"),
           ],
           selectedIndex: routeIndex,
--- lib/widgets/today_expenses.dart
+++ lib/widgets/today_expenses.dart
@@ -1,68 +1,85 @@
 import 'package:flutter/material.dart';
-
 import 'package:hive_flutter/adapters.dart';
 
 import 'package:spend_smart/models/expense.dart';
+import 'package:spend_smart/widgets/add_expense_form.dart';
+import 'package:spend_smart/widgets/data_manager.dart';
 
 import 'package:spend_smart/widgets/expense_list.dart';
 
-Box expenseBox = Hive.box('expenses');
-
 class TodayExpenses extends StatelessWidget {
-  const TodayExpenses({super.key});
+  TodayExpenses({super.key});
+
+  final DataManager dataManager = DataManager();
 
   @override
-  Widget build(BuildContext context) => ValueListenableBuilder(
-        valueListenable: expenseBox.listenable(),
-        builder: (context, value, child) => Container(
-          child: expenseBox.isEmpty
-              ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) {
+    final Box expenseBox = dataManager.expenseBox;
+    return ValueListenableBuilder(
+      valueListenable: expenseBox.listenable(keys: [dataManager.boxKey]),
+      builder: (context, value, child) {
+        int totalExpenses =
+            List<Expense>.from(expenseBox.get(dataManager.boxKey)).fold(
+                0, (previousValue, element) => previousValue + element.amount);
+        return Stack(
+          children: [
+            totalExpenses == 0
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/bonbon-brown-bear.png',
+                          width: 240,
+                        ),
+                        const SizedBox(height: 48),
+                        const Text(
+                          'No expenses',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                : Column(
                     children: [
-                      Image.asset(
-                        'assets/images/bonbon-brown-bear.png',
-                        width: 240,
-                      ),
-                      const SizedBox(height: 48),
-                      const Text(
-                        'No expenses',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                )
-              : Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: ValueListenableBuilder(
-                        valueListenable: expenseBox.listenable(),
-                        builder: (context, value, child) => Text(
-                          '₹ ${List<Expense>.from(value.values.toList()).fold(0, (previousValue, element) => previousValue + element.amount)}',
+                      Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: Text(
+                          '₹ $totalExpenses',
                           style: const TextStyle(
                             fontSize: 48,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: ExpenseList(
-                        onExpenseDeleted: () {
-                          ScaffoldMessenger.of(context).clearSnackBars();
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: const Text('Expense deleted.'),
-                              action: SnackBarAction(label: 'Undo', onPressed: () {}),
-                            ),
-                          );
-                        },
+                      Expanded(
+                        child: ExpenseList(),
+                      ),
+                    ],
+                  ),
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (dialogContext) => const AlertDialog(
+                      title: Text('Add expense'),
+                      content: SingleChildScrollView(
+                        child: AddExpenseForm(),
                       ),
                     ),
-                  ],
-                ),
-        ),
-      );
+                  );
+                },
+                label: const Text('Add expense'),
+                icon: const Icon(Icons.add_rounded),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
 }
