b18788f3343667d763a6c8bf650822475090b701
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-18 14:42:53 +0530

    Added functionality to work with `zeroExpense`

--- lib/widgets/Overview/overview.dart
+++ lib/widgets/Overview/overview.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'package:spend_smart/models/expense.dart';
+import 'package:spend_smart/widgets/Overview/overview_chart.dart';
 
 import 'package:spend_smart/widgets/Overview/overview_expenses_list.dart';
 import 'package:spend_smart/widgets/data_manager.dart';
@@ -14,46 +15,52 @@ class Overview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ValueListenableBuilder(
-      valueListenable: dataManager.expenseBox.listenable(),
-      builder: (context, value, child) {
-        List<List<Expense>> Overview = dataManager.expenseBox.values
-            .map((element) => List<Expense>.from(element))
-            .toList();
-        List<Map<String, dynamic>> expensesData = [];
-        for (List<Expense> expensesList in Overview) {
-          if (expensesList.isNotEmpty) {
-            expensesData.add({
-              'date': expensesList[0].formattedDate,
-              'totalTransactions': expensesList.length,
-              'totalAmount': expensesList.fold(
-                  0, (previousValue, element) => previousValue + element.amount)
-            });
-          } else {
-            break;
+        valueListenable: dataManager.expenseBox.listenable(),
+        builder: (context, value, child) {
+          List<List<Expense>> allExpenses = dataManager.expenseBox.values
+              .map((element) => List<Expense>.from(element))
+              .toList();
+          List<Map<String, dynamic>> expensesData = [];
+          for (List<Expense> expensesList in allExpenses) {
+            if (expensesList[0].amount == 0) {
+              expensesData.add({
+                'date': expensesList[0].date,
+                'totalTransactions': 0,
+                'totalAmount': 0
+              });
+            } else {
+              expensesData.add({
+                'date': expensesList[0].date,
+                'totalTransactions': expensesList.length,
+                'totalAmount': expensesList.fold(0,
+                    (previousValue, element) => previousValue + element.amount)
+              });
+            }
           }
-        }
-        return expensesData.isEmpty
-            ? Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/woolly-hand-with-mobile-phone.png',
-                      width: 200,
-                    ),
-                    const SizedBox(height: 36),
-                    const Text(
-                      'All your past expenses\n will be shown here',
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              )
-            : Column(children: [
-                Expanded(
-                    child: OverviewExpensesList(expensesData: expensesData)),
-              ]);
-      });
+          return expensesData.isEmpty
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/woolly-hand-with-mobile-phone.png',
+                        width: 200,
+                      ),
+                      const SizedBox(height: 36),
+                      const Text(
+                        'All your past expenses\n will be shown here',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                )
+              : Column(children: [
+                  OverviewChart(expensesData: expensesData),
+                  Expanded(
+                      child: OverviewExpensesList(expensesData: expensesData)),
+                ]);
+        },
+      );
 }
