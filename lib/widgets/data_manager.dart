6f13b784adcdad83d2f8285ee78b1081ac0055ec
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-18 14:32:31 +0530

    Added functionality to create a new box daily with `zeroExpense`

--- lib/widgets/data_manager.dart
+++ lib/widgets/data_manager.dart
@@ -9,14 +9,21 @@ class DataManager {
 
   double toDouble(TimeOfDay time) => time.hour + time.minute / 60.0;
 
-  String createBoxKey(DateTime date) {
+  String boxKey(DateTime date) {
     final String key = '${date.day}-${date.month}-${date.year}';
     return key;
   }
 
+  void createBoxIfAbsent(DateTime date) {
+    final String key = boxKey(date);
+    if (!expenseBox.containsKey(key)) {
+      expenseBox.put(key, [zeroExpense]);
+    }
+  }
+
   List<Expense> expenses(DateTime expenseDate) {
     List<Expense> expenseList =
-        List<Expense>.from(expenseBox.get(createBoxKey(expenseDate)) ?? []);
+        List<Expense>.from(expenseBox.get(boxKey(expenseDate)));
     return expenseList;
   }
 
@@ -30,15 +37,17 @@ class DataManager {
 
   void add(Expense expense) async {
     List<Expense> newExpenseList = expenses(expense.date);
+    if (newExpenseList[0].amount == 0) newExpenseList.clear();
     newExpenseList.add(expense);
     newExpenseList = sort(newExpenseList);
-    await expenseBox.put(createBoxKey(expense.date), newExpenseList);
+    await expenseBox.put(boxKey(expense.date), newExpenseList);
   }
 
   void remove(Expense expense) async {
     List<Expense> newExpenseList = expenses(expense.date);
     newExpenseList.remove(expense);
     newExpenseList = sort(newExpenseList);
-    await expenseBox.put(createBoxKey(expense.date), newExpenseList);
+    if (newExpenseList.isEmpty) newExpenseList.add(zeroExpense);
+    await expenseBox.put(boxKey(expense.date), newExpenseList);
   }
 }
