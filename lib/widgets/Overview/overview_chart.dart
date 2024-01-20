1eb89de307f9617a608a18df310cba29f81aed91
Saurabh Kumar <developer.saurabh@outlook.com>
2024-01-20 12:56:43 +0530

    Designed basic chart UI to show last 7 days of data

--- lib/widgets/Overview/overview_chart.dart
+++ lib/widgets/Overview/overview_chart.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:fl_chart/fl_chart.dart';
-import 'package:hive_flutter/adapters.dart';
-import 'package:spend_smart/widgets/data_manager.dart';
 
 class OverviewChart extends StatelessWidget {
   const OverviewChart({super.key, required this.expensesData});
@@ -10,42 +8,63 @@ class OverviewChart extends StatelessWidget {
   final List<Map<String, dynamic>> expensesData;
 
   @override
-  Widget build(BuildContext context) => ValueListenableBuilder(
-      valueListenable: DataManager().expenseBox.listenable(),
-      builder: (context, value, child) => Container(
-        margin: const EdgeInsets.only(top: 24),
-        padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-        child: AspectRatio(
-          aspectRatio: 1.7,
-          child: LineChart(
-            LineChartData(
-              minX: 1,
-              maxX: 30,
-              minY: 0,
-              lineBarsData: [
-                LineChartBarData(
-                  isCurved: true,
-                  isStrokeCapRound: true,
+  Widget build(BuildContext context) {
+    double d = 0;
+    final List<FlSpot> points = expensesData.length < 7
+        ? expensesData.reversed
+            .toList()
+            .map((e) => FlSpot(++d, (e['totalAmount'] as int).toDouble()))
+            .toList()
+        : expensesData.reversed
+            .toList()
+            .take(7)
+            .map((e) => FlSpot(++d, (e['totalAmount'] as int).toDouble()))
+            .toList();
+
+    return Container(
+      margin: const EdgeInsets.only(top: 56, right: 16),
+      child: AspectRatio(
+        aspectRatio: 1.7,
+        child: LineChart(
+          LineChartData(
+            minX: 1,
+            maxX: 7,
+            minY: 0,
+            lineBarsData: [
+              LineChartBarData(
+                isCurved: true,
+                isStrokeCapRound: true,
+                barWidth: 3,
+                dotData: const FlDotData(show: false),
+                belowBarData: BarAreaData(
+                  show: true,
                   gradient: LinearGradient(colors: [
-                    Theme.of(context).colorScheme.primary,
-                    Theme.of(context).colorScheme.secondary,
+                    Theme.of(context).colorScheme.primary.withOpacity(0.3),
+                    Theme.of(context)
+                        .colorScheme
+                        .secondaryContainer
+                        .withOpacity(0.3),
                   ]),
-                  spots: const [
-                    FlSpot(1, 300),
-                    FlSpot(2, 140),
-                    FlSpot(3, 325),
-                    FlSpot(4, 124),
-                    FlSpot(5, 45),
-                    FlSpot(6, 0),
-                  ],
-                )
-              ],
-              titlesData: const FlTitlesData(
-                topTitles: AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
                 ),
-                rightTitles: AxisTitles(
-                  sideTitles: SideTitles(showTitles: false),
+                gradient: LinearGradient(colors: [
+                  Theme.of(context).colorScheme.primary,
+                  Theme.of(context).colorScheme.secondaryContainer,
+                ]),
+                spots: points,
+              )
+            ],
+            titlesData: const FlTitlesData(
+              topTitles: AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
+              ),
+              rightTitles: AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
+              ),
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  interval: 1,
+                  reservedSize: 24,
                 ),
               ),
             ),
@@ -53,4 +72,5 @@ class OverviewChart extends StatelessWidget {
         ),
       ),
     );
+  }
 }
