e9b41c7f27d6b63ca501bacd9a89b36f81526ffa
Saurabh Kumar <developer.saurabh@outlook.com>
2024-02-15 20:39:26 +0530

    Added a basic `Drawer` and `SearchButton`.

--- lib/widgets/route_manager.dart
+++ lib/widgets/route_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spend_smart/settings.dart';
 
 import 'package:spend_smart/widgets/Overview/overview.dart';
 import 'package:spend_smart/widgets/Today/today.dart';
@@ -12,44 +13,71 @@ class RouteManager extends StatefulWidget {
 }
 
 class _RouteManagerState extends State<RouteManager> {
-  late FloatingActionButton addExpense;
   int routeIndex = 0;
   final List<Widget> routes = [TodayExpenses(), Overview()];
 
   @override
-  void initState() {
-    super.initState();
-    addExpense = FloatingActionButton(
-      child: const Icon(Icons.add_rounded),
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (dialogContext) => const AlertDialog(
-            title: Text('Add expense'),
-            content: SingleChildScrollView(
-              child: AddExpenseForm(),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Spend Smart'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.search_rounded),
+              onPressed: () {
+                showSearch(
+                    context: context, delegate: ExpensesSearchDelegate());
+              }),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.settings_outlined),
+              title: const Text('Settings'),
+              subtitle: const Text('Spend limit, etc.'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info_outline_rounded),
+              title: const Text('About'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       body: routes[routeIndex],
-      floatingActionButton: routeIndex == 0 ? addExpense : null,
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Add Expense'),
+        icon: const Icon(Icons.add_rounded),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (dialogContext) => const AlertDialog(
+              title: Text('Add expense'),
+              content: SingleChildScrollView(
+                child: AddExpenseForm(),
+              ),
+            ),
+          );
+        },
+      ),
       bottomNavigationBar: NavigationBar(
         destinations: const [
           NavigationDestination(
-              icon: Icon(Icons.receipt_outlined),
-              selectedIcon: Icon(Icons.receipt_rounded),
+              icon: Icon(Icons.account_balance_wallet_outlined),
+              selectedIcon: Icon(Icons.account_balance_wallet_rounded),
               label: 'Today'),
           NavigationDestination(
-              icon: Icon(Icons.receipt_long_outlined),
-              selectedIcon: Icon(Icons.receipt_long),
+              icon: Icon(Icons.insert_chart_outlined_rounded),
+              selectedIcon: Icon(Icons.insert_chart_rounded),
               label: 'Overview'),
         ],
         selectedIndex: routeIndex,
@@ -59,4 +87,34 @@ class _RouteManagerState extends State<RouteManager> {
       ),
     );
   }
+}
+
+class ExpensesSearchDelegate extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back_rounded),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+          icon: const Icon(Icons.clear_rounded),
+        ),
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) => Text('Value');
+
+  @override
+  Widget buildSuggestions(BuildContext context) => Text('Value');
 }
